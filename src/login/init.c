6255d8b62d2c06ae5611c5e10b40209a75078552
Andrei Karas <akaras@inbox.ru>
2014-11-05 22:19:04 +0300

    Add empty login server plugin.

---
+++ src/login/init.c
@@ -0,0 +1,38 @@
+// Copyright (c) Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// Copyright (c) 2014 Evol developers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../../common/HPMi.h"
+#include "../../../common/malloc.h"
+#include "../../../common/mmo.h"
+#include "../../../common/socket.h"
+#include "../../../common/strlib.h"
+#include "../../../login/login.h"
+
+#include "../../../common/HPMDataCheck.h" /* should always be the last file included! (if you don't make it last, it'll intentionally break compile time) */
+
+HPExport struct hplugin_info pinfo =
+{
+    "evol_login",
+    SERVER_TYPE_LOGIN,
+    "0.1",
+    HPM_VERSION
+};
+
+/* run when server starts */
+HPExport void plugin_init (void) {
+    /* core interfaces */
+    iMalloc = GET_SYMBOL("iMalloc");
+}
+
+HPExport void server_preinit (void) {
+}
+
+HPExport void server_online (void) {
+}
+
+HPExport void plugin_final (void) {
+}
