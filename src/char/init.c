5cb297ff979cd4805fbed7521e15f4c00e179ac0
Andrei Karas <akaras@inbox.ru>
2014-11-17 17:22:59 +0300

    char: init all interfaces.

--- src/char/init.c
+++ src/char/init.c
@@ -10,6 +10,21 @@
 #include "../../../common/mmo.h"
 #include "../../../common/socket.h"
 #include "../../../common/strlib.h"
+#include "../../../char/char.h"
+#include "../../../char/geoip.h"
+#include "../../../char/int_auction.h"
+#include "../../../char/int_elemental.h"
+#include "../../../char/int_guild.h"
+#include "../../../char/int_homun.h"
+#include "../../../char/int_mail.h"
+#include "../../../char/int_mercenary.h"
+#include "../../../char/int_party.h"
+#include "../../../char/int_pet.h"
+#include "../../../char/int_quest.h"
+#include "../../../char/int_storage.h"
+#include "../../../char/inter.h"
+#include "../../../char/loginif.h"
+#include "../../../char/mapif.h"
 
 #include "common/interfaces.h"
 
@@ -26,6 +41,22 @@ HPExport struct hplugin_info pinfo =
 HPExport void plugin_init (void)
 {
     interfaces_init_common();
+
+    chr = GET_SYMBOL("chr");
+    geoip = GET_SYMBOL("geoip");
+    inter_auction = GET_SYMBOL("inter_auction");
+    inter_elemental = GET_SYMBOL("inter_elemental");
+    inter_guild = GET_SYMBOL("inter_guild");
+    inter_homunculus = GET_SYMBOL("inter_homunculus");
+    inter_mail = GET_SYMBOL("inter_mail");
+    inter_mercenary = GET_SYMBOL("inter_mercenary");
+    inter_party = GET_SYMBOL("inter_party");
+    inter_pet = GET_SYMBOL("inter_pet");
+    inter_quest = GET_SYMBOL("inter_quest");
+    inter_storage = GET_SYMBOL("inter_storage");
+    inter = GET_SYMBOL("inter");
+    loginif = GET_SYMBOL("loginif");
+    mapif = GET_SYMBOL("mapif");
 }
 
 HPExport void server_preinit (void)
