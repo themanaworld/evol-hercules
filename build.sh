b399f265bd766484570c1a2bcc9823a32ac12f20
Andrei Karas <akaras@inbox.ru>
2014-11-17 12:54:23 +0300

    In build script add command line options.
    ./build.sh     - build with sanity flags.
    ./build.sh old - build without special flags.

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+CMD="$1"
+
+if [[ -z "${CMD}" ]]; then
+    export CMD="default"
+fi
+
 mkdir build
 # this need for some outdated os
 mkdir m4
 autoreconf -i
 cd build
-../configure --enable-sanitize
+if [[ "${CMD}" == "default" ]]; then
+    ../configure --enable-sanitize
+elif [[ "${CMD}" == "old" ]]; then
+    ../configure
+fi
 make -j3
+export RET=$?
 cd -
-exit $?
+exit $RET
