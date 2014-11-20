8c7713fd898900e7afe936e647c051f0a92e07e1
Andrei Karas <akaras@inbox.ru>
2014-11-20 15:13:10 +0300

    Add lang.c with translation related functionality.

---
+++ src/map/lang.h
@@ -0,0 +1,17 @@
+// Copyright (c) Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// Copyright (c) 2014 Evol developers
+
+#ifndef EVOL_MAP_LANG
+#define EVOL_MAP_LANG
+
+extern struct DBMap *translate_db;
+
+void do_init_langs (void);
+
+const char* lang_trans(const char *str, int lng, int flg);
+
+const char* lang_pctrans(const char *str, TBL_PC *sd);
+
+int lang_getId(const char *str);
+
+#endif  // EVOL_MAP_LANG
