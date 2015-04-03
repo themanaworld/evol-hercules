fd83c8bda2a4508ddf734e00c57b5699d4ea9cb7
Andrei Karas <akaras@inbox.ru>
2015-04-04 00:25:47 +0300

    map: fix compilation error.

--- src/map/pc.c
+++ src/map/pc.c
@@ -209,13 +209,13 @@ int epc_isuseequip_post(int retVal, struct map_session_data *sd, int *nPtr)
             sd->battle_status.int_ < data->requiredInt ||
             sd->battle_status.dex < data->requiredDex ||
             sd->battle_status.luk < data->requiredLuk ||
-            sd->battle_status.maHp < data->requiredMaxHp ||
-            sd->battle_status.maSp < data->requiredMaxSp ||
+            sd->battle_status.max_hp < data->requiredMaxHp ||
+            sd->battle_status.max_sp < data->requiredMaxSp ||
             sd->battle_status.batk < data->requiredAtk ||
             sd->battle_status.matk_min < data->requiredMAtkMin ||
             sd->battle_status.matk_max < data->requiredMAtkMax ||
             sd->battle_status.def < data->requiredDef ||
-            sd->battle_status.mdef < data->requiredMDef ||
+            sd->battle_status.mdef < data->requiredMDef
         )
         {
             return 0;
