cdeb65b6bcbce16c654891a7f75b86bc669e2bdf
Andrei Karas <akaras@inbox.ru>
2015-10-01 19:33:26 +0300

    Add wrong parameters list in addflooritem function.

--- src/emap/map.c
+++ src/emap/map.c
@@ -26,8 +26,8 @@
 #include "emap/struct/itemdext.h"
 #include "emap/struct/sessionext.h"
 
-int emap_addflooritem_post(const struct block_list *bl,
-                           int retVal,
+int emap_addflooritem_post(int retVal,
+                           const struct block_list *bl,
                            struct item *item,
                            int *amount __attribute__ ((unused)),
                            int16 *m __attribute__ ((unused)),
