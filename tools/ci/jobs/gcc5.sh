05e96fb6a893ce3bd5e5846e27d0807aa808257c
Andrei Karas <akaras@inbox.ru>
2016-04-28 01:11:55 +0300

    Clone server code in build ci script.

--- tools/ci/jobs/gcc5.sh
+++ tools/ci/jobs/gcc5.sh
@@ -7,10 +7,16 @@ export LOGFILE=gcc5.log
 source ./tools/ci/scripts/init.sh
 
 aptget_install gcc-5 g++-5 \
+    git-core \
     make autoconf automake autopoint gettext libphysfs-dev \
     libxml2-dev libcurl4-gnutls-dev libpng-dev \
     libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-net1.2-dev libsdl-ttf2.0-dev
 
 do_init
+clone_servercode
+mkdir server-code/src/evol
+cp -r evol-hercules/* ../server-code/src/evol
+cd ../server-code/src/evol
+
 run_configure $*
 run_make
