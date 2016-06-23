d2e38e0dc95108adef3c5944ba5b710ebeab6a20
Andrei Karas <akaras@inbox.ru>
2016-06-24 00:59:15 +0300

    Add gcc snapshot and gcc 4.6 and different flags combinations.

---
+++ tools/ci/jobs/gccsnapshot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export CC=gcc
+export CXX=g++
+export LOGFILE=gcc-snapshot.log
+export PATH=/usr/lib/gcc-snapshot/bin:$PATH
+
+source ./tools/ci/scripts/init.sh
+
+aptget_install gcc-snapshot \
+    git-core \
+    make autoconf automake autopoint \
+    libtool libmysqlclient-dev libz-dev libpcre3-dev
+
+do_init
+build_init
+
+run_configure $*
+run_make
