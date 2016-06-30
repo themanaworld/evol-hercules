ae99c9c0850b24dc7302e1384b6773eb2c4f93f1
Andrei Karas <akaras@inbox.ru>
2016-06-30 17:41:03 +0300

    Add 5 unused id for status effects

--- tools/vars.sh
+++ tools/vars.sh
@@ -2,7 +2,9 @@
 
 # MAX_SKILL    1478  + 22 = 1500
 # MAX_SKILL_ID 10015 + 22 = 10037
+# SC_MAX       642   + 5  = 647
+# SI_MAX       966   + 5  = 971
 
 # can be used for custom skill id: 10016 - 10036
 
-export VARS="-DMAX_SKILL=1500 -DMAX_SKILL_ID=10037"
+export VARS="-DMAX_SKILL=1500 -DMAX_SKILL_ID=10037 -DSC_MAX=647 -DSI_MAX=971"
