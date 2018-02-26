db09bd0f8be1c465359742af695079437241d101
Andrei Karas <akaras@inbox.ru>
2018-02-27 01:18:11 +0300

    Add clang-7 builds into .gitlab-ci.yml

---
+++ tools/ci/jobs/clang7.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export CC=clang-7.0
+export CXX=clang++-7.0
+export LOGFILE=clang6.log
+
+source ./tools/ci/scripts/init.sh
+
+aptget_install clang-7.0 \
+    git-core \
+    make autoconf automake autopoint \
+    libtool libmysqlclient-dev libz-dev libpcre3-dev
+
+do_init
+build_init
+
+run_configure $*
+run_make
