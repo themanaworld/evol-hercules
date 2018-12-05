6cf162853caa324cd8fb3b27296bc20ee17a04f6
Andrei Karas <akaras@inbox.ru>
2018-12-05 23:56:53 +0300

    Fix deps for clang-3.9 ci build.

--- tools/ci/jobs/clang39.sh
+++ tools/ci/jobs/clang39.sh
@@ -9,7 +9,7 @@ source ./tools/ci/scripts/init.sh
 aptget_install clang-3.9 \
     git-core \
     make autoconf automake autopoint \
-    libtool libmysqlclient-dev libz-dev libpcre3-dev
+    libtool mariadb-client libmariadbclient-dev-compat libz-dev libpcre3-dev
 
 do_init
 build_init
