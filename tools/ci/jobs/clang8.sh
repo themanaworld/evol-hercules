4e33254976fefcd4b16ccedeaace444e3e1f2926
Andrei Karas <akaras@inbox.ru>
2019-08-19 19:29:58 +0300

    Add new compilers and fix issues with old compilers in .gitlab-ci.yml

--- tools/ci/jobs/clang8.sh
+++ tools/ci/jobs/clang8.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export CC=clang-7
-export CXX=clang++-7
-export LOGFILE=clang7.log
+export CC=clang-8
+export CXX=clang++-8
+export LOGFILE=clang8.log
 
 source ./tools/ci/scripts/init.sh
 
-aptget_install clang-7 \
+aptget_install clang-8 \
     git-core \
     make autoconf automake autopoint \
     libtool libmysqlclient-dev libz-dev libpcre3-dev
--- tools/ci/jobs/gcc9.sh
+++ tools/ci/jobs/gcc9.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export CC=clang-7
-export CXX=clang++-7
-export LOGFILE=clang7.log
+export CC=gcc-9
+export CXX=g++-9
+export LOGFILE=gcc9.log
 
 source ./tools/ci/scripts/init.sh
 
-aptget_install clang-7 \
+aptget_install gcc-9 \
     git-core \
     make autoconf automake autopoint \
     libtool libmysqlclient-dev libz-dev libpcre3-dev
